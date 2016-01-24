d481c2e980e1331cf279f5504892668890d4909b
Merter Sualp <merter.sualp@gmail.com>
2016-01-24 21:24:44 +0200

    First Hadoop code

---
+++ LikedJamCount.java
@@ -0,0 +1,61 @@
+
+package tr.name.sualp.merter.hadoop.myjam;
+
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class LikedJamCount {
+
+  public static class TokenizerMapper
+      extends Mapper<Object, Text, Text, IntWritable>{
+
+      private final static IntWritable one = new IntWritable(1);
+
+      public void map(Object key, Text value, Context context
+          ) throws IOException, InterruptedException {
+        String[] userAndJam = value.toString().split("\t");
+        context.write(new Text(userAndJam[1]), one);
+          }
+  }
+
+  public static class IntSumReducer
+      extends Reducer<Text,IntWritable,Text,IntWritable> {
+      private IntWritable result = new IntWritable();
+
+      public void reduce(Text key, Iterable<IntWritable> values,
+          Context context
+          ) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+          sum += val.get();
+        }
+        result.set(sum);
+        context.write(key, result);
+          }
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "like count");
+    job.setJarByClass(LikedJamCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+}
+
