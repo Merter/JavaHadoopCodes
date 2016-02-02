13d8d2c3d0490fdce47bc865e1a3074a4e940c02
Merter Sualp <merter.sualp@gmail.com>
2016-02-02 23:06:59 +0200

    Hadoop cluster calistirmaya gerek kalmayan bir Driver kodu yazdim.

---
+++ LikedJamCountDriver.java
@@ -0,0 +1,38 @@
+
+package tr.name.sualp.merter.hadoop.myjam;
+
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class LikedJamCountDriver extends Configured implements Tool {
+
+
+
+
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "like count");
+    job.setJarByClass(LikedJamCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+}
+
