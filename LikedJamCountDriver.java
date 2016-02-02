dcd4499149d6ee0c09756aa5ba2eea5358fb1092
Merter Sualp <merter.sualp@gmail.com>
2016-02-02 23:07:58 +0200

    Driver kodu eksikti, duzelttim.

--- LikedJamCountDriver.java
+++ LikedJamCountDriver.java
@@ -13,25 +13,33 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class LikedJamCountDriver extends Configured implements Tool {
 
+  public int run(String[] args) throws Exception {
+    Configuration conf = getConf();
+    Job job = Job.getInstance(conf, "ljc-driver");
+    job.setJarByClass(getClass());
 
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));    
 
+    job.setMapperClass(LikedJamCount.TokenizerMapper.class);
+    job.setReducerClass(LikedJamCount.IntSumReducer.class);
 
-
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "like count");
-    job.setJarByClass(LikedJamCount.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    // Let ToolRunner handle generic command-line options 
+    int exitCode = ToolRunner.run(new Configuration(), new LikedJamCountDriver(), args);
+    System.exit(exitCode);
   }
 
 }
